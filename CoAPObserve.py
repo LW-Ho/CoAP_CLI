2cf67f2b9b8a62501c4d97363c83fad256c1f2a0
LW-Ho <leoleo41306@gmail.com>
2018-10-17 16:18:35 +0800

    Refactory code.
    Try the new function.

---
+++ CoAPObserve.py
@@ -0,0 +1,64 @@
+import subprocess, sys
+import os
+import signal
+import threading
+
+from MoteData import MoteData
+import logging
+log = logging.getLogger("CoAP Observe")
+
+class CoAPObserve(threading.Thread):
+  def __init__(self, node, resource, port=5683, kwargs=None, object_callback=None)
+  threading.Thread.__init__(self,name=node)
+    self.coap_client = None
+    self.node = node
+    self.resource = resource
+    self.port = port
+    self.object_callback = object_callback
+    return
+
+  def message_callback(self, response):
+        """
+        :type response: coapthon.messages.response.Response
+        """
+        if response is not None:
+            log.debug("Got new message")
+            if log.isEnabledFor(logging.DEBUG):
+                packet_content = ":".join("{:02x}".format(ord(c)) for c in response.payload)
+                log.debug(packet_content)
+            log.debug("Payload length: {0}".format(len(response.payload)))
+            mote_data = MoteData.make_from_bytes(response.source[0], response.payload)
+            if mote_data is not None and self.object_callback is not None:
+                self.object_callback(mote_data)
+            log.debug("=================================")
+
+  def run(self):
+    log.info("CoAP Observe \"{0}\" started.".format(self.name))
+    self.coap_client = HelperClient(server=(self.host, self.port))
+    self.coap_client.observe(path=resource, callback=self.message_callback)
+    
+    # get_cmd = 'coap -o \"coap://['+self.node+']:5683/g/'+self.resource+'\"'
+    # try:
+    #   self.coapProcess = subprocess.Popen(get_cmd, stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
+    # except:
+    #   print "Not success for send out.\n"
+    #   pass
+
+  def stop(self):
+    log.info("Stoping CoAP Observe \"{0}\" .".format(self.name))
+    if self.coap_client is not None:
+      self.coap_client.stop()
+
+    # try:
+    #   os.killpg(os.getpgid(self.coapProcess.pid), signal.SIGTERM)
+    #   self._is_running = False
+    # except:
+    #   print "Error of terminate()"
+    #   return
+    
+  def printName(self):
+    log.info("Node Name : {0}".format(self.node))
+
+  def getName(self):
+    return self.node
+    
