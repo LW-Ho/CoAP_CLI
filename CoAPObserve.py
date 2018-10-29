14fa37f84184a713bcd93e8b5b56d5f919ab3a30
LW-Ho <leoleo41306@gmail.com>
2018-10-29 15:12:58 +0800

    Bugs Fix.

--- CoAPObserve.py
+++ CoAPObserve.py
@@ -32,13 +32,13 @@ class CoAPObserve(threading.Thread):
             log.debug("=================================")
           
             # will upload data to mysql server.
-            try :
-              mote_data = MoteData.make_from_bytes(response.source[0], response.payload)
-              if mote_data is not None and self.object_callback is not None:
-                  self.object_callback(mote_data)
-            except :
-              self.stdout.write("Unexpected error:", sys.exc_info()[0])
-              self.stdout.write("\n")
+          try :
+            mote_data = MoteData.make_from_bytes(response.source[0], response.payload)
+            if mote_data is not None and self.object_callback is not None:
+                self.object_callback(mote_data)
+          except :
+            self.stdout.write("Unexpected error:", sys.exc_info()[0])
+            self.stdout.write("\n")
 
   def run(self):
     log.info("CoAP Observe \"{0}\" started.".format(self.name))
