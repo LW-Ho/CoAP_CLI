a5d69d0b427f4edd9ef7f8dd2e87bdcff19168a4
LW-Ho <leoleo41306@gmail.com>
2018-11-29 16:49:58 +0800

    Fix bugs.

--- AutoOb.py
+++ AutoOb.py
@@ -3,7 +3,7 @@ from coapthon.client.helperclient import HelperClient
 from CoAPObserve import CoAPObserve
 
 class AutoOb(threading.Thread):
-  def __init__(self, node=None, mote_lists, mote_observe_lists, group=None, target=None, verbose=None, autoOb_callback=None, object_callback=None):
+  def __init__(self, mote_lists, mote_observe_lists, group=None, target=None, verbose=None, autoOb_callback=None, object_callback=None):
     threading.Thread.__init__(self, group=group, target=target, naem=node, verbose=verbose)
     self.auto_ob = None
     self.record_counter = 0
