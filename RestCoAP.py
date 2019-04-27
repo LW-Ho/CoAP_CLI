168088152639b712cc6c94a6d2e66b48eb6357f7
LW-Ho <leoleo41306@gmail.com>
2019-04-27 19:24:44 +0800

    Update.

--- RestCoAP.py
+++ RestCoAP.py
@@ -5,26 +5,21 @@ import sys
 import time
 from coapthon.client.helperclient import HelperClient
 # coap get "coap://[fd00::212:4b00:615:a736]:5683/g/sht21?pp=2&thd=20"
-from MoteData import MoteData
 port = 5683
-local_queue_numbers = 1
 
 def postQueryToNode(node,resource,query):
   query = "?"+query
   resource = "res/"+resource+query
-  flag = 0
-  while flag == 0 :
-    try:
-      coap_client = HelperClient(server=(node, port))
-      start = time.time()
-      coap_client.post(path=resource, payload='' ,timeout=60)
-      coap_client.stop()
-      elapsed = time.time() - start
-      print "%s  successful delivery, %.2f seconds." %(node, elapsed)
-      flag = 1
-    except:
-      coap_client.stop()
-      print node+" did not successfully send out, retry..."
+  try:
+    coap_client = HelperClient(server=(node, port))
+    start = time.time()
+    coap_client.post(path=resource, payload='' ,timeout=120)
+    coap_client.close()
+    elapsed = time.time() - start
+    print "%s  successful delivery, %.2f seconds." %(node, elapsed)
+  except:
+    coap_client.close()
+    print node+" did not successfully send out."
 
 def postToAllNode(List,resource,query):
   query = "?"+query
@@ -34,14 +29,13 @@ def postToAllNode(List,resource,query):
     try:
       coap_client = HelperClient(server=(node, port))
       start = time.time()
-      #coap_client.post(resource, '')
       coap_client.post(path=resource, payload='' ,timeout=60)
-      coap_client.stop()
+      coap_client.close()
       elapsed = time.time() - start
       print "%s  successful delivery, %.2f seconds." %(node, elapsed)
 
     except:
-      coap_client.stop()
+      coap_client.close()
       print node+" did not successfully send out."
       pass
 
